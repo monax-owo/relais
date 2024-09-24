f4bfa676873b30b784de75381cc9d604741e3156
zprl <zprl.dev@gmail.com>
2024-09-24 09:35:31 +0900

    remove test

--- src-tauri/tests/dialog_test.rs
+++ src-tauri/tests/dialog_test.rs
@@ -1,7 +1,7 @@
 use tauri::test::{mock_builder, mock_context, noop_assets};
 
-#[serial_test::serial]
-#[test]
-fn dialog() {
+// #[serial_test::serial]
+// #[test]
+fn _dialog_test() {
   mock_builder().build(mock_context(noop_assets())).unwrap();
 }
